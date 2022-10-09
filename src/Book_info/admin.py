f95493acd7b3de3ff41701e627d56a8055a4d97d
Your Name~ <you@example.com>
2022-10-09 15:01:41 +0300

    Models_admin

--- src/Book_info/admin.py
+++ src/Book_info/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Authors)
+admin.site.register(models.Genre)
+admin.site.register(models.Publisher)
+admin.site.register(models.Series)
