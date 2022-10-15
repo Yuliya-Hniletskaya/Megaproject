9add67eab47d59d34b21554abcb4053a839e9dcd
Your Name~ <you@example.com>
2022-10-15 13:58:54 +0300

    auth1 shell

--- src/Book_info/admin.py
+++ src/Book_info/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from . import models
+from Book_info import models
+from Book_info.models import Author
 
 # Register your models here.
-
-admin.site.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'date_of_birth', 'date_of_death')
+    
+admin.site.register(models.Author, AuthorAdmin)
 admin.site.register(models.Genre)
 admin.site.register(models.Publisher)
 admin.site.register(models.Series)
